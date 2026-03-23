e945b95f15f167988c4e404b16a643d26128effa
tallywker990w <nonet2773@gmail.com>
2026-03-22 22:01:39 -0400

    Clean up Main.cpp by removing unused code
    Removed unused includes and the SaveScript function.

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -1,18 +1,7 @@
 #include <jni.h>
 #include <string>
-#include <fstream>
-#include <sys/stat.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_android_support_Launcher_setTitle(JNIEnv *env, jclass clazz) {
     return env->NewStringUTF("Prism Menu");
-}
-
-void SaveScript(std::string name, std::string content) {
-    std::string path = "/storage/emulated/0/Documents/Prism/Scripts/";
-    mkdir("/storage/emulated/0/Documents/Prism/", 0777);
-    mkdir(path.c_str(), 0777);
-    std::ofstream file(path + name + ".lua");
-    file << content;
-    file.close();
 }
